c0f9cbafd1c23ee098af1b1d013468ebf7457f5c
Dono <150925284+xDonobait@users.noreply.github.com>
2025-07-17 21:44:56 -0500

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,9 @@
+document.getElementById("openSidebarBtn").onclick = function () {
+  document.getElementById("sidebar").style.width = "250px";
+  document.getElementById("openSidebarBtn").style.display = "none";
+};
+
+document.getElementById("closeSidebarBtn").onclick = function () {
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("openSidebarBtn").style.display = "block";
+};
